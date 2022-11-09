69d58ba5d86f9d2a4ef3f96e7a631d3d742cf696
jyj <jiangyuanji@126.com>
2022-11-09 23:50:55 +0800

    进行了页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>gsh document</div>
+  <div class="container">
+    <header><h1>GSH Document</h1></header>
+    <aside>list list</aside>
+    <main>main frame</main>
+    <footer>this is the footer</footer>
+  </div>
 </template>
 
 <script>
@@ -7,9 +12,32 @@ export default {};
 </script>
 
 <style lang="less" scoped>
-section {
-  div {
-    color: red;
-  }
+* {
+}
+h1 {
+  text-align: center;
+  font-size: 1.8rem;
+}
+.container {
+  display: grid;
+  grid-template: 5rem auto 1.5rem / 17rem auto;
+  height: 100vh;
+  width: 100vw;
+}
+header {
+  background: dodgerblue;
+  grid-area: 1 / 1 / span 1 / span 1;
+}
+aside {
+  background: green;
+  grid-area: 2 / 1 / span 1 / span 1;
+}
+main {
+  background: red;
+  grid-area: 1 / 2 / span 2 / span 1;
+}
+footer {
+  background: gold;
+  grid-area: 3 / 1 / span 1 / span 2;
 }
 </style>
