17d7df0c1059a6df33bb356472d6b42b0ac2cf55
jyj <jiangyuanji@126.com>
2022-11-09 22:31:56 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>gsh document</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+section {
+  div {
+    color: red;
+  }
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslintPlugin from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        charset: false,
+        // additionalData: '@import "./src/assets/css/global.less"', // 全局less文件，可选
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    eslintPlugin({
+      include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue'],
+    }),
+  ],
+});
